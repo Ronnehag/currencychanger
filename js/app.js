adcefcec13750ae2183e6269e1303594c64a0178
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-03-07 11:27:59 +0100

    Added fetchmethods for country and data

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 
 
+(function() {
+
+    // EVENT HANDLERS
+    const fromCurrencyInput = document.querySelector("#currencyFrom");
+    const toCurrencyInput = document.querySelector("#currencyTo");
+
+
+
+
+
+})();
--- js/fetchData.js
+++ js/fetchData.js
@@ -13,7 +13,20 @@ const fetchData = () => {
 
     }
 
-    function fetchData(){
+    function fetchCountryList(){
+        const _url = "http://www.apilayer.net/api/list?access_key=2ac1611a2dfc48d014d02f69297a99c8&format=1";
+        return async function(){
+            try{
+                let res = await fetch(_url);
+                let data = await res.json();
+                return data;
+            } catch(err){
+                console.log(err);
+            }
+        }
+    }
+
+    function fetchCurrencyData(){
         const _url = "http://www.apilayer.net/api/live?access_key=2ac1611a2dfc48d014d02f69297a99c8&format=1";
         return async function(){
             try{
