c69eb79ab2e31f8aca0490d88d0453ad4da046a7
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-03-07 10:52:17 +0100

    added styling and main body for the app.

--- js/fetchData.js
+++ js/fetchData.js
@@ -16,16 +16,13 @@ const fetchData = () => {
     function fetchData(){
         const _url = "http://www.apilayer.net/api/live?access_key=2ac1611a2dfc48d014d02f69297a99c8&format=1";
         return async function(){
-            let data;
             try{
-                let res = await fetch(url);
-                data = await res.json();
+                let res = await fetch(_url);
+                let data = await res.json();
+                return data;
             } catch(err){
                 console.log(err);
             }
-            return data;
         }
-    }
-
-    
+    }    
 }
