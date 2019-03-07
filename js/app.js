ec34a254393ff4b73fcb773c7590ca3fa52cd1c5
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-03-07 14:18:58 +0100

    added css to style card component and background.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,34 @@
 
 
-(function() {
-
+(function () {
     // Initialize the app, starts the interval timer, 
     // fetches API data and stores in localstorage.
     // Program will work towards the localstorage, the API will be updated in the background once / hour.
     fetchData.init();
 
-    // EVENT HANDLERS
+    // CACHED DOM
     const fromCurrencyInput = document.querySelector("#currencyFrom");
     const toCurrencyInput = document.querySelector("#currencyTo");
     const dropdownList = document.querySelector("#countries");
 
-    function fillDropDown(){
-        const countries = fetchdata.getCountries();
+    // EVENTLISTENERS
+    fromCurrencyInput.addEventListener("change",calculateValues);
+
+    // Fills the search input's dropdown list with values from all countries
+    // Data is fetched from localstorage
+    (function fillDropDown() {
+        const { currencies } = fetchData.getCountries();
+        for (var prop in currencies) {
+            let temp = `<option value="${currencies[prop]}"
+            style="background-image:url(https://www.countryflags.io/be/flat/24.png)">            
+            </option>`
+            dropdownList.innerHTML += temp;
+        }
+    })();
+
+    function calculateValues(){
+        console.log(this);
     }
-    
 
 
 })();
