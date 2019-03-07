3c9547706c23859a580e1b055eda57e22a1c37d8
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-03-07 13:46:35 +0100

    added recursive timeout call for fetch data.

--- js/fetchData.js
+++ js/fetchData.js
@@ -4,12 +4,6 @@ const fetchData = (function () {
 
     const storage = window.localStorage;
 
-
-    async function updateLocalStorage() {
-        setInterval(await fetchCountryList, 1000 * 60 * 60);
-        setInterval(await fetchCurrencyData, 1000 * 60 * 60);
-    }
-
     const getCountryJSON = () => {
         return JSON.parse(storage.getItem("countryList"));
     }
@@ -17,40 +11,40 @@ const fetchData = (function () {
         return JSON.parse(storage.getItem("currencyValues"));
     }
 
-    function fetchCountryList() {
+    async function fetchCountryList() {
         const _url = "http://www.apilayer.net/api/list?access_key=2ac1611a2dfc48d014d02f69297a99c8&format=1";
-        return async function () {
-            try {
-                let res = await fetch(_url);
-                let data = await res.json();
-                storage.setItem("countryList", JSON.stringify(data));
-            } catch (err) {
-                console.log(err);
-            }
+        try {
+            let res = await fetch(_url);
+            let data = await res.json();
+            storage.setItem("countryList", JSON.stringify(data));
+        } catch (err) {
+            console.log(err);
         }
+        setTimeout(fetchCountryList, 1000 * 60 * 60);
     }
 
-    function fetchCurrencyData() {
+    async function fetchCurrencyData() {
         const _url = "http://www.apilayer.net/api/live?access_key=2ac1611a2dfc48d014d02f69297a99c8&format=1";
-        return async function () {
-            try {
-                let res = await fetch(_url);
-                let data = await res.json();
-                storage.setItem("currencyValues", JSON.stringify(data));
-            } catch (err) {
-                console.log(err);
-            }
+        try {
+            let res = await fetch(_url);
+            let data = await res.json();
+            storage.setItem("currencyValues", JSON.stringify(data));
+        } catch (err) {
+            console.log(err);
         }
+        setTimeout(fetchCurrencyData, 1000 * 60 * 60);
     }
 
-    const init = async () => {
-        updateLocalStorage();
+
+    const init = () => {
+        fetchCountryList();
+        fetchCurrencyData();
     }
 
     return {
         init: init,
-        getCountries : getCountryJSON,
-        getCurrency : getCurrencyDataJSON
+        getCountries: getCountryJSON,
+        getCurrency: getCurrencyDataJSON
     }
 
 })();
