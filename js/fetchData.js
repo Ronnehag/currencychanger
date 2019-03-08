5ff8233e03b97ce10e073e6f6815e797089d6bd4
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-03-08 14:48:45 +0100

    added mobile support

--- js/fetchData.js
+++ js/fetchData.js
@@ -2,28 +2,66 @@
 const fetchData = (function () {
     const storage = window.localStorage;
 
-    const getCountryJSON = () => {
-        if (storage.getItem("countryList") != null) {
-            return JSON.parse(storage.getItem("countryList"));
+    function detectmob() {
+        if (navigator.userAgent.match(/Android/i)
+            || navigator.userAgent.match(/webOS/i)
+            || navigator.userAgent.match(/iPhone/i)
+            || navigator.userAgent.match(/iPad/i)
+            || navigator.userAgent.match(/iPod/i)
+            || navigator.userAgent.match(/BlackBerry/i)
+            || navigator.userAgent.match(/Windows Phone/i)
+        ) {
+            return true;
         }
         else {
-            fetchCountryList().then(() => {
-                return JSON.parse(storage.getItem("countryList"));
-            });
+            return false;
         }
     }
+    const getCountryJSON = () => {
 
-    async function getCountryName(name){
-        let alpha2Code = name.substr(0,2);
-        try{
+        if (detectmob()) {
+            fetchMobile().then(val => {
+                return val;
+            });
+        } else {
+            if (storage.getItem("countryList") != null) {
+                return JSON.parse(storage.getItem("countryList"));
+            }
+            else {
+                fetchCountryList().then(() => {
+                    return JSON.parse(storage.getItem("countryList"));
+                });
+            }
+        }
+
+
+    }
+
+    async function getCountryName(name) {
+        let alpha2Code = name.substr(0, 2);
+        try {
             let res = await fetch(`https://restcountries.eu/rest/v2/alpha/${alpha2Code}`);
             let data = res.json();
             return data;
         }
-        catch(err){
+        catch (err) {
             console.log(err);
         }
     }
+
+    async function fetchMobile() {
+        const _url = `https://api.exchangeratesapi.io/latest`;
+        try {
+            let res = await fetch(_url);
+            let data = await res.json();
+            return data
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+
+
 
     async function fetchCountryList() {
         const _url = `https://api.exchangeratesapi.io/latest`;
@@ -55,7 +93,7 @@ const fetchData = (function () {
         init: init,
         getCountries: getCountryJSON,
         fetchCurrency: fetchCurrencyData,
-        getCountryName : getCountryName
+        getCountryName: getCountryName
     }
 
 })();
